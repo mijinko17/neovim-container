dc41a9b5e63279865d1a477676b755786c747393
mijinko <kougouseikeidansi@gmail.com>
2023-08-06 20:35:09 +0900

    ittan komito.

--- nvim.sh
+++ nvim.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 readonly absolute_path=$(readlink -f $1)
+readonly absolute_current_dir=$(pwd)
 readonly regex="${HOME}/(.+)"
 
 if [[ $absolute_path =~ $regex ]]; then
   readonly neovim_target="/home/host/${BASH_REMATCH[1]}"
-  docker run --rm --name neovim-container -it -v $HOME:/home/host --network=host mijinko17/neovim-container:latest nvim $neovim_target
+  # if [[ $pwd =~ $regex ]]; then
+  #   echo ${BASH_REMATCH[1]}
+  # end
+  # readonly neovim_target="/home/host/${BASH_REMATCH[1]}"
+  docker run --rm --name neovim-container -it -v $HOME:/home/host -w /home/host --network=host mijinko17/neovim-container:latest nvim $neovim_target
 else
   echo "not matched"
 fi
