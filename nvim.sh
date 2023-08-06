415e23a42e045e12984d24b9e9c0d533159249ef
mijinko <kougouseikeidansi@gmail.com>
2023-08-06 21:57:17 +0900

    Refactor nvim.sh.

--- nvim.sh
+++ nvim.sh
@@ -6,22 +6,24 @@ readonly current_directory_absolute_path_in_host=$(pwd)
 function relative_path_from_home_directory() {
   readonly regex="${HOME}/(.+)"
   if [[ $1 =~ $regex ]]; then
-   echo ${BASH_REMATCH[1]}
-   return 0
+    echo ${BASH_REMATCH[1]}
+    return 0
   else
     return 1
   fi
 }
 
-if ! neovim_target_relative_path=$(relative_path_from_home_directory $neovim_target_absolute_path_in_host); then
-  echo $?
+if ! readonly current_directory_relative_path=$(relative_path_from_home_directory $current_directory_absolute_path_in_host); then
+  echo "You are not in home directory."
+  exit 1
 fi
-neovim_target_absolute_path_in_container=/home/host/$neovim_target_relative_path
+readonly current_directory_absolute_path_in_containier=/home/host/$current_directory_relative_path
 
-if ! current_directory_relative_path=$(relative_path_from_home_directory $current_directory_absolute_path_in_host); then
-  echo $?
+if ! readonly neovim_target_relative_path=$(relative_path_from_home_directory $neovim_target_absolute_path_in_host); then
+  echo "Target file does not exist in home directory."
+  exit 1
 fi
-current_directory_absolute_path_in_containier=/home/host/$current_directory_relative_path
+readonly neovim_target_absolute_path_in_container=/home/host/$neovim_target_relative_path
 
 docker run \
   --rm \
