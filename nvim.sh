7302ca2462283e390de7e7cf6e13a34f67dc97eb
mijinko <kougouseikeidansi@gmail.com>
2023-08-06 21:00:34 +0900

    komi

--- nvim.sh
+++ nvim.sh
@@ -10,6 +10,11 @@ if [[ $absolute_path =~ $regex ]]; then
     readonly absolute_current_dir_in_container="/home/host/${BASH_REMATCH[1]}"
     docker run \
       --rm \
-      --name neovim-container -it -v $HOME:/home/host -w $absolute_current_dir_in_container --network=host mijinko17/neovim-container:latest nvim $neovim_target
+      --name neovim-container \
+      -it \
+      -v $HOME:/home/host \
+      -w $absolute_current_dir_in_container \
+      --network=host mijinko17/neovim-container:latest \
+      nvim $neovim_target
   fi
 fi
