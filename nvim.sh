ca7c1cd6dd097abf09c3cf78c1418884438d2a53
mijinko <kougouseikeidansi@gmail.com>
2023-08-12 12:15:25 +0900

    Recognize the las arg as file path.

--- nvim.sh
+++ nvim.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#readonly neovim_target_absolute_path_in_host=$(readlink -f $1)
+readonly neovim_target_absolute_path_in_host=$(readlink -f "${@:$#:1}")
 readonly current_directory_absolute_path_in_host=$(pwd)
 readonly container_name=neovim-container
 readonly container_name_regex=/neovim-container$
 
 function relative_path_from_home_directory() {
-  readonly regex="${HOME}/(.+)"
+  local -r regex="${HOME}/(.+)"
   if [[ $1 =~ $regex ]]; then
     echo ${BASH_REMATCH[1]}
     return 0
@@ -26,7 +26,7 @@ function upgrade(){
 
 neovim_opt=''
 
-while [ $# -gt 0 ]; do
+while [ $# -gt 1 ]; do
   case $1 in
     --upgrade ) upgrade;exit ;;
     *) neovim_opt="$neovim_opt $1 $2"
@@ -34,17 +34,19 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-if ! readonly current_directory_relative_path=$(relative_path_from_home_directory $current_directory_absolute_path_in_host); then
+if ! current_directory_relative_path=$(relative_path_from_home_directory $current_directory_absolute_path_in_host); then
   echo "You are not in home directory."
   exit 1
 fi
 readonly current_directory_absolute_path_in_containier=/home/host/$current_directory_relative_path
 
-#if ! readonly neovim_target_relative_path=$(relative_path_from_home_directory $neovim_target_absolute_path_in_host); then
-#  echo "Target file does not exist in home directory."
-#  exit 1
-#fi
-#readonly neovim_target_absolute_path_in_container=/home/host/$neovim_target_relative_path
+if ! neovim_target_relative_path=$(relative_path_from_home_directory $neovim_target_absolute_path_in_host); then
+  echo "Target file does not exist in home directory."
+  exit 1
+fi
+readonly neovim_target_absolute_path_in_container=/home/host/$neovim_target_relative_path
+
+neovim_opt="$neovim_opt $neovim_target_absolute_path_in_container"
 
 if [ "$(docker ps -aq -f name="$container_name_regex")" ]; then
     docker start $container_name &> /dev/null
