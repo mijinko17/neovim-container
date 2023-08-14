bfcae4edd246de81de5df4b2489ee2afc8eb2e7a
mijinko <kougouseikeidansi@gmail.com>
2023-08-14 11:47:37 +0900

    Show log for changing uid.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,15 @@ uid=$(stat -c "%u" .)
 gid=$(stat -c "%g" .)
 
 if [ "$uid" -ne 0 ]; then
-    if [ "$(id -g $USER)" -ne $gid ]; then
-        getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
-        chgrp -R $gid $HOME
-    fi
-    if [ "$(id -u $USER)" -ne $uid ]; then
-        usermod -u $uid $USER
-    fi
+  echo Change UID.
+  if [ "$(id -g $USER)" -ne $gid ]; then
+    getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
+    chgrp -R $gid $HOME
+  fi
+  if [ "$(id -u $USER)" -ne $uid ]; then
+    usermod -u $uid $USER
+  fi
+  echo Finish changing UID.
 fi
 
 exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
