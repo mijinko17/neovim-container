a4674dad9bd4bdade1c9acea4d97df132d612f3d
mijinko <kougouseikeidansi@gmail.com>
2024-04-07 08:22:26 +0000

    Impl NvimCommandBuilder.

--- src/container_runner.rs
+++ src/container_runner.rs
@@ -1,17 +1,44 @@
 use std::process::Command;
 
 pub fn run_container() {
-    Command::new("docker")
-        .arg("run")
-        .arg("--rm")
-        .arg("--interactive")
-        .arg("--tty")
-        .args(vec!["--workdir", "/"])
-        .arg("--network=host")
-        .arg("mijinko17/neovim-container:latest")
-        .arg("nvim")
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
+    NvimCommandExecutor {
+        image: "mijinko17/neovim-container:latest",
+        volumes: vec![],
+        work_dir: "/".to_string(),
+    }
+    .execute();
+}
+
+struct VolumeArg {
+    host_path: String,
+    container_path: String,
+}
+
+struct NvimCommandExecutor {
+    image: &'static str,
+    volumes: Vec<VolumeArg>,
+    work_dir: String,
+}
+
+impl NvimCommandExecutor {
+    pub fn execute(self) {
+        self.volumes
+            .into_iter()
+            .fold(
+                Command::new("docker")
+                    .arg("run")
+                    .arg("--rm")
+                    .arg("--interactive")
+                    .arg("--tty")
+                    .args(vec!["--workdir", self.work_dir.as_str()])
+                    .arg("--network=host"),
+                |acc, cur| acc.args(vec![cur.host_path, cur.container_path]),
+            )
+            .arg(self.image)
+            .arg("nvim")
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+    }
 }
