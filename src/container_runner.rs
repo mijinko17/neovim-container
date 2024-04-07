54c007904ab9b2cec531d81a383953ed54ab8206
mijinko <kougouseikeidansi@gmail.com>
2024-04-07 07:38:20 +0000

    Execute docker command.

--- src/container_runner.rs
+++ src/container_runner.rs
@@ -1,5 +1,17 @@
 use std::process::Command;
 
 pub fn run_container() {
-    Command::new("vi").spawn().unwrap().wait().unwrap();
+    Command::new("docker")
+        .arg("run")
+        .arg("--rm")
+        .arg("--interactive")
+        .arg("--tty")
+        .args(vec!["--workdir", "/"])
+        .arg("--network=host")
+        .arg("mijinko17/neovim-container:latest")
+        .arg("nvim")
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
 }
