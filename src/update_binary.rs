16ba98160be7b5fbb280fd47a57632bbfd7dd37c
mijinko <kougouseikeidansi@gmail.com>
2024-04-11 18:50:24 +0000

    Use ubuntu-20.04 image to build gnu binary.

--- src/update_binary.rs
+++ src/update_binary.rs
@@ -5,7 +5,6 @@ pub fn update_binary() -> Result<(), Box<dyn (::std::error::Error)>> {
         .repo_owner("mijinko17")
         .repo_name("neovim-container")
         .bin_name("neovim-container")
-        .target(BINARY_TARGET)
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
         .build()?
@@ -14,9 +13,9 @@ pub fn update_binary() -> Result<(), Box<dyn (::std::error::Error)>> {
     Ok(())
 }
 
-#[cfg(target_os = "linux")]
-static BINARY_TARGET: &str = "x86_64-unknown-linux-musl";
-#[cfg(target_os = "macos")]
-static BINARY_TARGET: &str = "-x86_64-apple-darwin";
-#[cfg(not(any(target_os = "linux", target_os = "macos")))]
-static BINARY_TARGET: &str = "x86_64-unknown-linux-musl";
+// #[cfg(target_os = "linux")]
+// static BINARY_TARGET: &str = "x86_64-unknown-linux-musl";
+// #[cfg(target_os = "macos")]
+// static BINARY_TARGET: &str = "-x86_64-apple-darwin";
+// #[cfg(not(any(target_os = "linux", target_os = "macos")))]
+// static BINARY_TARGET: &str = "x86_64-unknown-linux-musl";
