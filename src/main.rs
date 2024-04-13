955495bb94daa7c494ab8c96db4ff1ef08c43fd3
mijinko <kougouseikeidansi@gmail.com>
2024-04-13 15:34:22 +0000

    Chore.

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,6 @@ use crate::cli::{Args, RawArgs};
 use crate::container_runner::run_container;
 
 fn main() -> Result<()> {
-    // let path = Path::new("/home/vscode/hoge");
-    // match unistd::mkfifo(path, stat::Mode::S_IRWXU) {
-    //     Ok(_) => println!("created {:?}", path),
-    //     Err(err) => println!("Error creating fifo: {}", err),
-    // }
-    // print_pid("second");
-    // std::thread::spawn(move || loop {
-    //     let mut file = fs::File::create(path).unwrap();
-    //     file.write_all(b"hogehoge").unwrap();
-    //     print_pid("spawned second");
-    // });
     let args = Args::from(RawArgs::parse());
     if args.update {
         update_binary()
