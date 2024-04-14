7c28f99bf9dadc132a47cd0ab217f05cf61efeff
mijinko <kougouseikeidansi@gmail.com>
2024-04-14 19:12:23 +0000

    Add telescope.

--- image/.config/nvim/init.lua
+++ image/.config/nvim/init.lua
@@ -1,20 +1,5 @@
 require('core')
--- require('plugin')
+require('plugin')
 require('clipboard')
 require('filetype')
 require('terminal')
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
---
+++ image/.config/nvim/lua/plugins/hybrid-material.lua
@@ -0,0 +1,7 @@
+return {
+  'kristijanhusak/vim-hybrid-material',
+  config = function()
+    vim.cmd 'colorscheme hybrid_material'
+  end,
+}
+
