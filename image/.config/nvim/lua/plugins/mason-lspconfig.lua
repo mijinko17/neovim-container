e3c398bc84b01c032f039086b7644b7611456f1f
mijinko <kougouseikeidansi@gmail.com>
2024-04-15 20:02:09 +0000

    Use config = true.

--- image/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ image/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,5 @@
 return {
   "williamboman/mason-lspconfig.nvim",
   dependencies = { "williamboman/mason.nvim" },
-  config = function()
-    require("mason-lspconfig").setup()
-  end
+  config = true
 }
