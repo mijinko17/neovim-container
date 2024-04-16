e8897113d2927bd3333ea5cb1f63da2b92debe7e
mijinko <kougouseikeidansi@gmail.com>
2024-04-16 19:04:48 +0000

    Create directory for telekasten.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod container_config;
 mod container_runner;
 mod directory_state;
 mod path;
+mod telekasten;
 mod terminal_command;
 mod update_binary;
 
@@ -14,6 +15,7 @@ use clap::Parser;
 use clipboard::{clean_named_pipe, setup_clipboard};
 use directory_state::DirectoryStateProviderImpl;
 use rand::random;
+use telekasten::setup_for_telekasten;
 use update_binary::update_binary;
 
 use crate::cli::{Args, RawArgs};
@@ -24,6 +26,7 @@ fn main() -> Result<()> {
     if args.update {
         update_binary()
     } else {
+        let _ = setup_for_telekasten(&DirectoryStateProviderImpl);
         let container_name = random_contaniner_name();
         let result = setup_clipboard(&DirectoryStateProviderImpl, &container_name)
             .and_then(|_| run_container(args, DirectoryStateProviderImpl, &container_name));
---
+++ src/telekasten.rs
@@ -0,0 +1,7 @@
+use anyhow::Result;
+
+use crate::directory_state::DirectoryStateProvider;
+
+pub fn setup_for_telekasten(dir_state: &impl DirectoryStateProvider) -> Result<()> {
+    dir_state.create_directory(&dir_state.home_dir()?.join("zettelkasten"))
+}
