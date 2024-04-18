add1c29868d3c5b0fbd11bb36fd8fc1b0451484e
mijinko <kougouseikeidansi@gmail.com>
2024-04-18 19:43:05 +0000

    Proto of reading yaml.

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,34 @@ mod telekasten;
 mod terminal_command;
 mod update_binary;
 
+use std::{collections::HashMap, path::Path};
+
 use action::{pull_image, run_container, update_binary};
 use anyhow::Result;
 use clap::Parser;
 use constants::UID;
 use container_config::{image_name, ContainerImageConfig};
+use directory_state::{DirectoryStateProvider, DirectoryStateProviderImpl};
 use rand::random;
+use serde::{Deserialize, Serialize};
 
 use crate::cli::{Args, RawArgs};
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Service {
+    image: String,
+    volumes: Option<Vec<String>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Compose {
+    services: HashMap<String, Service>,
+}
+
 fn main() -> Result<()> {
+    let compose_yaml = DirectoryStateProviderImpl.file_content(&Path::new("compose.yml"))?;
+    let deserialized: Compose = serde_yaml::from_str(&compose_yaml)?;
+    println!("{:?}", deserialized);
     let args = Args::from(RawArgs::parse());
     if args.update {
         update_binary()
